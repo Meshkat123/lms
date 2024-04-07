856611bee8fdc95f6366c2d7165a095db7bd95b6
Nayeem Abdul Qaiyum <qaiyum.1416@gmail.com>
2024-04-08 01:28:25 +0600

    copied

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 $adminNamespace     = 'App\Http\Controllers\Admin';
 
+
 Route::namespace($adminNamespace)->group(function () {
     Route::get('', 'DashboardController@index');
     Route::get('admin/home', 'DashboardController@index')->name('dashboard');
